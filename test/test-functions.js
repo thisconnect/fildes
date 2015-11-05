d94b85a03d8d6de1b3a95727c37231ff4b4ae983
thisconnect <hi@this-connect.me>
2015-11-05 15:58:14 +0100

    test function length

--- test/test-functions.js
+++ test/test-functions.js
@@ -3,6 +3,7 @@ var file = require('../');
 var tape = require('tape');
 
 // https://top.fse.guru/nodejs-a-quick-optimization-advice-7353b820c92e#.ytqibx71s
+
 tape('function length below 600', function(t){
     var length;
     for (var method in file){

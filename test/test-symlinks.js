04b2ac78f2e8722ebd5ed6193bf6bbec69811d89
thisconnect <hi@this-connect.me>
2015-11-27 05:24:36 +0100

    test symlink dir, no support for symlink type

--- test/test-symlinks.js
+++ test/test-symlinks.js
@@ -9,6 +9,8 @@ var dest1 = resolve(__dirname, './data/symlink.txt');
 var dest2 = resolve(__dirname, './data/symlink/dest.txt');
 var filepath1 = resolve(__dirname, './data/symlink/symlink.txt');
 var filepath2 = resolve(__dirname, './data/symlink/symlink2.txt');
+var dir1 = resolve(__dirname, './data/symlink/dir1');
+var dir2 = resolve(__dirname, './data/symlink/dir2');
 
 
 tape('setup symlink', function(t){
@@ -24,7 +26,39 @@ tape('symlink', function(t){
         return file.readFile(filepath1, {encoding: 'utf8'});
     })
     .then(function(content){
-        t.equal(content, 'symlink test\n', 'symlink has content of dest1')
+        t.equal(content, 'symlink test\n', 'symlink has content of dest1');
+        t.end();
+    })
+    .catch(function(error){
+        t.error(error);
+        t.end();
+    });
+});
+
+
+tape('symlink dir', function(t){
+    var file1 = resolve(dir1, 'file.txt');
+    var file2 = resolve(dir2, 'file.txt');
+
+    file.mkdir(dir1)
+    .then(function(){
+        return file.symlink(dir1, dir2);
+    })
+    .then(function(){
+        return file.writeFile(file1, 'test in dir1');
+    })
+    .then(function(){
+        t.pass('dir symlinked');
+        return file.readdir(dir2);
+    })
+    .then(function(files){
+        t.ok(Array.isArray(files), 'files is Array');
+        t.ok(files.length == 1, 'has some files');
+        t.equal(files[0], 'file.txt', 'dir 2 has file.txt');
+        return file.readFile(file2, {encoding: 'utf8'});
+    })
+    .then(function(content){
+        t.equal(content, 'test in dir1', 'has same content');
         t.end();
     })
     .catch(function(error){
